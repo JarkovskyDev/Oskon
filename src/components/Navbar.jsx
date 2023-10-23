cb1094370de3664a8aa0a9fc04e53eb7125d6d7c
Dušan Jarkovský <jarkovsky.dev@gmail.com>
2023-10-23 22:53:50 +0200

    Navbar almost finished

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,83 @@
+import React, {useState} from 'react'
+import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from "react-icons/fa"
+import {HiOutlineMail} from "react-icons/hi"
+import {BsFillPersonLinesFill} from "react-icons/bs"
+import Logo from "../assets/logo.png"
+import {Link} from 'react-scroll'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4'>
+        <div>
+            <a href="/">
+                <img src={Logo} alt="Logo Oskon s.r.o." style={{width:"50px"}} />
+            </a>
+        </div>
+
+        <ul className='hidden md:flex'>
+            <li>
+                <Link to='home' smooth={true} duration={500}>
+                    Úvod
+                </Link>
+            </li>
+            <li>
+                <Link to='for_home' smooth={true} duration={500}>
+                    Domácnost
+                </Link>
+            </li> 
+            <li>
+                <Link to='for_industry' smooth={true} duration={500}>
+                    Průmysl
+                </Link>
+            </li>
+            <li>
+                <Link to='welding' smooth={true} duration={500}>
+                    Svařování
+                </Link>
+            </li>
+            <li>
+                <Link to='contacts' smooth={true} duration={500}>
+                    Kontakt
+                </Link>
+            </li>
+        </ul>
+
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+
+        <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen flex flex-col justify-center items-center"}>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+                    Úvod
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to='for_home' smooth={true} duration={500}>
+                    Domácnosti
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to='for_industry' smooth={true} duration={500}>
+                    Průmysl
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to='welding' smooth={true} duration={500}>
+                    Svařování
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to='contacts' smooth={true} duration={500}>
+                    Kontakt
+                </Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
