d368940c1b8ef2aa64921e4caed6719a0b978a35
Dušan Jarkovský <jarkovsky.dev@gmail.com>
2023-11-05 23:24:02 +0100

    Image added

--- src/components/HelmetSettings.jsx
+++ src/components/HelmetSettings.jsx
@@ -6,7 +6,7 @@ const HelmetSettings = () => {
     <div>
         <Helmet>
             <html lang="cs" />
-            <title>Oskon s.r.o.</title>
+            <title>Oskon s.r.o - Kovovýroba a strojní obrábění</title>
             <meta name="description" content="Společnost Oskon s.r.o. se zabývá kovovýrobou, strojním obráběním kovů a svařováním. Dále se zabýváme výrobou specializovaných strojích přípravků." />
         </Helmet>
     </div>
