3b60879affb15efd20dcc9e7055b39ff35993101
Dušan Jarkovský <jarkovsky.dev@gmail.com>
2023-11-14 21:45:09 +0100

    finish

--- src/components/Engineering.jsx
+++ src/components/Engineering.jsx
@@ -13,7 +13,7 @@ const Engineering = () => {
           <h3 className='text-5xl font-bold max-sm:mt-2 sm:mt-2'>
             <span className='border-t-2 border-[#ea5b0c]'>S</span>TROJÍRENSTV<span className='border-b-2 border-[#ea5b0c]'>Í</span>
           </h3>
-          <p className='text-xl text-justify pb-6 mt-2'>Oskon s.r.o je předním jménem v oblasti strojírenství a výroby jednoúčelových strojních přípravků. Naše společnost je specializovaná na precizní kovovýrobu a nabízí inovativní řešení pro strojírenské a průmyslové potřeby. Naši zkušení inženýři a technici pracují na vývoji a výrobě špičkových jednoúčelových přípravků, které maximalizují efektivitu a přesnost vaší výroby. S důrazem na kvalitu, spolehlivost a technickou dokonalost jsme vaším partnerem pro kovové výrobky, které zlepšují výkonnost a konkurenceschopnost vaší strojírenské a výrobní činnosti.</p>
+          <p className='text-xl text-justify pb-6 mt-2 '>Oskon s.r.o je předním jménem v oblasti strojírenství a výroby jednoúčelových strojních přípravků. Naše společnost je specializovaná na precizní kovovýrobu a nabízí inovativní řešení pro strojírenské a průmyslové potřeby. Naši zkušení inženýři a technici pracují na vývoji a výrobě špičkových jednoúčelových přípravků, které maximalizují efektivitu a přesnost vaší výroby. S důrazem na kvalitu, spolehlivost a technickou dokonalost jsme vaším partnerem pro kovové výrobky, které zlepšují výkonnost a konkurenceschopnost vaší strojírenské a výrobní činnosti.</p>
         </div>
         <div className='grid grid-cols-2 grid-rows-6 h-[80vh]'>
           <img className='row-span-3 object-cover w-full h-full p-2' src={image1} alt="/" />
